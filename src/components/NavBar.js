4b8322d7243b37e681674571c3ce98010a17214e
Avanish shukla <36558094+avishukl96@users.noreply.github.com>
2024-03-30 10:57:02 +0530

    News page almost completed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
 export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              NewMonkey
-            </a>
+        <nav className="navbar navbar-expand-lg  fixed-top navbar-dark bg-dark">
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              News Headlines
+            </Link>
             <button
               className="navbar-toggler"
               type="button"
@@ -25,15 +25,51 @@ export class Navbar extends Component {
               id="navbarSupportedContent"
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
+                {/* <li className="nav-item">
+                  <Link className="nav-link " aria-current="page" to="/">
                     Home
-                  </a>
+                  </Link>
+                </li> */}
+                {/* <li className="nav-item">
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                </li> */}
+                <li className="nav-item">
+                  <Link className="nav-link" to="/">
+                    General
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/about">
-                    About
-                  </a>
+                  <Link className="nav-link" to="/business">
+                    Business
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/entertainment">
+                    Entertainment
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link className="nav-link" to="/health">
+                    Health
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/science">
+                    Science
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/sports">
+                    Sports
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/technology">
+                    Technology
+                  </Link>
                 </li>
               </ul>
             </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
 export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 8,
+    category: "general",
+  };
+
+  static protoTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.int,
+    category: PropTypes.string,
+  };
+
   constructor(props) {
     super(props);
     this.state = {
@@ -13,8 +26,8 @@ export class News extends Component {
     };
   }
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=504d970796a04919a54abc3670f8ae36&page=1&pageSize=${this.state.pageSize}`;
+  async updateNews() {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=504d970796a04919a54abc3670f8ae36&page=1&pageSize=${this.state.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parseData = await data.json();
@@ -23,52 +36,78 @@ export class News extends Component {
       articles: parseData.articles,
       loading: false,
       totalResults: parseData.totalResults,
-      loading: false,
     });
   }
 
+  async componentDidMount() {
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=504d970796a04919a54abc3670f8ae36&page=1&pageSize=${this.state.pageSize}`;
+    // this.setState({ loading: true });
+    // let data = await fetch(url);
+    // let parseData = await data.json();
+    // //console.log(parseData.articles);
+    // this.setState({
+    //   articles: parseData.articles,
+    //   loading: false,
+    //   totalResults: parseData.totalResults,
+    //   loading: false,
+    // });
+    this.updateNews();
+  }
+
   handleNextClick = async () => {
-    if (
-      !(
-        this.state.page + 1 >
-        Math.ceil(this.state.totalResults / this.state.pageSize)
-      )
-    ) {
-      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=504d970796a04919a54abc3670f8ae36&page=${
-        this.state.page + 1
-      }&&pageSize=${this.state.pageSize}`;
-      this.setState({ loading: true });
-      let data = await fetch(url);
-      let parseData = await data.json();
-      //console.log(parseData.articles);
-      this.setState({
-        articles: parseData.articles,
-        loading: false,
-        page: this.state.page + 1,
-        loading: false,
-      });
-    }
+    // if (
+    //   !(
+    //     this.state.page + 1 >
+    //     Math.ceil(this.state.totalResults / this.state.pageSize)
+    //   )
+    // ) {
+    //   let url = `https://newsapi.org/v2/top-headlines?country=${
+    //     this.props.country
+    //   }&category=${
+    //     this.props.category
+    //   }&apiKey=504d970796a04919a54abc3670f8ae36&page=${
+    //     this.state.page + 1
+    //   }&&pageSize=${this.state.pageSize}`;
+    //   this.setState({ loading: true });
+    //   let data = await fetch(url);
+    //   let parseData = await data.json();
+    //   //console.log(parseData.articles);
+    //   this.setState({
+    //     articles: parseData.articles,
+    //     loading: false,
+    //     page: this.state.page + 1,
+    //     loading: false,
+    //   });
+    // }
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
   };
   handlePrevClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=504d970796a04919a54abc3670f8ae36&page=${
-      this.state.page - 1
-    }&pageSize=${this.state.pageSize}`;
-    this.setState({ loading: true });
-    let data = await fetch(url);
-    let parseData = await data.json();
-    //console.log(parseData.articles);
-    this.setState({
-      articles: parseData.articles,
-      loading: false,
-      page: this.state.page - 1,
-      loading: false,
-    });
+    // let url = `https://newsapi.org/v2/top-headlines?country=${
+    //   this.props.country
+    // }&category=${
+    //   this.props.category
+    // }&apiKey=504d970796a04919a54abc3670f8ae36&page=${
+    //   this.state.page - 1
+    // }&pageSize=${this.state.pageSize}`;
+    // this.setState({ loading: true });
+    // let data = await fetch(url);
+    // let parseData = await data.json();
+    // //console.log(parseData.articles);
+    // this.setState({
+    //   articles: parseData.articles,
+    //   loading: false,
+    //   page: this.state.page - 1,
+    //   loading: false,
+    // });
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
   };
 
   render() {
     return (
       <div className="container my-3">
-        <h2 className="my-4 text-center">NewsMonkey - Top Headlines</h2>
+        <h2 className="my-4 text-center">Top News Headlines</h2>
         {this.state.loading && <Spinner />}
         <div className="row">
           {!this.state.loading &&
@@ -81,7 +120,7 @@ export class News extends Component {
                     title={element.title ? element.title.slice(0, 35) : ""}
                     description={
                       element.description
-                        ? element.description.slice(0, 50)
+                        ? element.description.slice(0, 100)
                         : ""
                     }
                     imageURL={
@@ -90,6 +129,9 @@ export class News extends Component {
                         : "https://findusonweb.com/files/master/10423.jpg"
                     }
                     newsURL={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
                   />
                 </div>
               );
